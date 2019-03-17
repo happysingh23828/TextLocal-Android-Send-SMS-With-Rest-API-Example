43f92be34a0d2e00524af2596c0e64c402ede15f
Happy Singh <happy.singh@talview.com>
2019-03-17 17:04:18 +0530

    Removed API key and Dummy NUmber Validation

--- app/src/main/java/com/androchef/kisansms/ui/sendmessage/SendMessageDialogViewModel.kt
+++ app/src/main/java/com/androchef/kisansms/ui/sendmessage/SendMessageDialogViewModel.kt
@@ -27,8 +27,6 @@ class SendMessageDialogViewModel : ViewModel() {
 
     fun sendMessage(postTextLocalSMSData: PostTextLocalSMSData) {
         state = SendMessageDialogState(loading = true, message = "Sending..")
-
-        if(checkItIsNotDummyNumber(postTextLocalSMSData.numbers)){
             APIUtils.textLocalApi().sendMessage(getHashMapFromPojo(postTextLocalSMSData))
                 .enqueue(object : Callback<TextLocalSMSRespose> {
                     override fun onFailure(call: Call<TextLocalSMSRespose>, t: Throwable) {
@@ -43,21 +41,13 @@ class SendMessageDialogViewModel : ViewModel() {
                         )
                     }
                 })
-        } else{
-            state = SendMessageDialogState(loading = false, sendFailure = true, message = "This is Dummy number \n Please select KISSAN Testing Phone to check Cases from List\n")
-        }
-
-    }
-
-    private fun checkItIsNotDummyNumber(number: String?): Boolean {
-        return number!!.contains("+91-9971792703")
     }
 
     private fun getHashMapFromPojo(postTextLocalSMSData: PostTextLocalSMSData): HashMap<String, Any> {
         val map = HashMap<String, Any>()
         map[API_KEY] = postTextLocalSMSData.apiKey!!
         map[NUMBERS] = postTextLocalSMSData.numbers!!.replace("+91-","91")
-        map[TEST] = postTextLocalSMSData.test!!
+        map[TEST] = true
         map[MESSAGE] = postTextLocalSMSData.message!!
         return map
     }
